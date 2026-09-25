b30051ca160d565e48b3926c03c98973b075e4a1
a-lost-n <alostn30@gmail.com>
2026-09-25 16:55:10 -0500

    public version

--- archetype_encoder/variables.py
+++ archetype_encoder/variables.py
@@ -1,19 +1,15 @@
-TOUR_NAME = "25_worlds"
-RK9_TOUR_ID = "WCS01w2Vf7wv855Yvhir"
-POKEDATA_TOUR_ID = "0000167"
-LIMITLESS_TOUR_ID = "6069"
-LIMITLESS_LABS_TOUR_ID = "0035"
-DAY1_ROUNDS = 9
+TOUR_NAME = "27_baltimore"
+POKEDATA_TOUR_ID = "0000213"
+LIMITLESS_LABS_TOUR_ID = "0072"
 
 
 CATEGORY = 'masters'
-FORMAT_EXP = "bw"
+FORMAT_EXP = "pbl"
 
 
 LIMITLESS_BASE_ENDPOINT = "https://limitlesstcg.com"
-LIMITLESS_TOUR_ENDPOINT  = f"{LIMITLESS_BASE_ENDPOINT}/tournaments/{LIMITLESS_TOUR_ID}"
 LIMITLESS_DECKS_ENDPOINT = f"{LIMITLESS_BASE_ENDPOINT}/decks?variants=true&show=100&time=all&type=all&format=svi-{FORMAT_EXP}&region=all&division=all"
-LIMILESS_LABS_BASE_ENDPOINT = "https://mew.limitlesstcg.com/labs/data/standings?tournamentId={}&division={}"
-RK9_BASE_ENDPOINT = "https://rk9.gg"
-RK9_ROSTER = f"{RK9_BASE_ENDPOINT}/roster/{RK9_TOUR_ID}"
+LIMITLESS_LABS_BASE_ENDPOINT = "https://mew.limitlesstcg.com/labs/data/tcg/standings?tournamentId={}&division={}"
+# RK9_BASE_ENDPOINT = "https://rk9.gg"
+# RK9_ROSTER = f"{RK9_BASE_ENDPOINT}/roster/{RK9_TOUR_ID}"
 POKEDATA_CSV = f'https://pokedata.ovh/standings/{POKEDATA_TOUR_ID}/{CATEGORY}/data.csv?'
---
+++ makefile
@@ -0,0 +1,2 @@
+clean:
+	rm -f image_cropper/report/*.png; rm -f image_cropper/uncropped_report/*.png
